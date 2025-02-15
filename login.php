ddda50e740e57f1632514312fcbf29968d850ac5
Ahmed Hassan <7moody63600@gmail.com>
2025-02-16 01:12:51 +0200

    Update login.php
    Fixed spacing issues

--- login.php
+++ login.php
@@ -44,10 +44,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     ?>
     <form method="POST" action="login.php">
-        <label>Username:</label><br>
-        <input type="text" name="username" required><br>
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br>
+        <label>Username:</label>
+        <input type="text" name="username" required>
+        <label>Password:</label>
+        <input type="password" name="password" required>
         <button type="submit">Login</button>
     </form>
 </body>
