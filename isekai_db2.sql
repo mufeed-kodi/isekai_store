6352f98c477ddd069cdb1be0d6461c3594debf4e
Ahmed Hassan <7moody63600@gmail.com>
2025-02-15 17:11:51 +0200

    Update to isekai_db2.sql
    I added a phone_number field and an address field in the users table, added few rows to test it.

--- isekai_db2.sql
+++ isekai_db2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 15, 2025 at 02:14 AM
+-- Generation Time: Feb 15, 2025 at 03:06 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `admins` (
 --
 
 INSERT INTO `admins` (`id`, `username`, `password`, `created_at`, `last_login`) VALUES
-(2, 'fido', '$2y$10$421InEA/aq4FBi1Djj/tMOAEXpeaURclEu6wEjrpJsbtdPouhA.y2', '2025-02-04 21:49:13', '2025-02-14 23:13:36');
+(2, 'fido', '$2y$10$421InEA/aq4FBi1Djj/tMOAEXpeaURclEu6wEjrpJsbtdPouhA.y2', '2025-02-04 21:49:13', '2025-02-15 13:04:28');
 
 -- --------------------------------------------------------
 
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `cart` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -101,7 +101,20 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `status` varchar(50) DEFAULT 'Pending',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total`, `order_date`, `status`) VALUES
+(1, 3, 119.97, '2025-02-15 13:06:30', 'Pending'),
+(2, 5, 999.99, '2025-02-15 13:42:32', 'Delivered'),
+(3, 5, 119.97, '2025-02-15 13:42:55', 'Delivered'),
+(4, 5, 1599.60, '2025-02-15 13:57:24', 'Delivered'),
+(5, 5, 99999999.99, '2025-02-15 13:58:32', 'Delivered'),
+(6, 5, 1039.98, '2025-02-15 14:51:42', 'Pending'),
+(7, 5, 1039.98, '2025-02-15 14:52:06', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -119,7 +132,22 @@ CREATE TABLE IF NOT EXISTS `order_items` (
   PRIMARY KEY (`id`),
   KEY `order_id` (`order_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
+(1, 1, 1, 3, 39.99),
+(2, 2, 2, 1, 999.99),
+(3, 3, 1, 3, 39.99),
+(4, 4, 1, 40, 39.99),
+(5, 5, 2, 738385, 999.99),
+(6, 6, 1, 1, 39.99),
+(7, 6, 2, 1, 999.99),
+(8, 7, 1, 1, 39.99),
+(9, 7, 2, 1, 999.99);
 
 -- --------------------------------------------------------
 
@@ -139,14 +167,15 @@ CREATE TABLE IF NOT EXISTS `products` (
   `stock` int DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `category_id`, `name`, `description`, `price`, `image`, `created_at`, `stock`) VALUES
-(1, 3, 'Python book', 'Programming Python: Powerful Object-Oriented Programming', 39.99, '71wvDBsPRSL.png', '2025-02-15 01:50:53', 10);
+(1, 3, 'Python book', 'Programming Python: Powerful Object-Oriented Programming', 39.99, '71wvDBsPRSL.png', '2025-02-15 01:50:53', 46),
+(2, 1, 'A45', 'A very beautiful phone owned by nekura', 999.99, 'A45.jpg', '2025-02-15 13:13:53', 98);
 
 -- --------------------------------------------------------
 
@@ -159,20 +188,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fullname` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
+  `phone_number` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `address` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`, `created_at`) VALUES
-(1, 'Mufeed Mamadan', 'mufeed', 'mufeed.kodika@gmail.com', '$2y$10$baJyF06Oq7UGdKeKl3oCR.4wfNFgRSOxifhYyKkw8/kiry68ZJ80u', '2025-02-04 22:43:01');
+INSERT INTO `users` (`id`, `fullname`, `username`, `phone_number`, `address`, `email`, `password`, `created_at`) VALUES
+(1, 'Mufeed Mamadan', 'mufeed', '+256744370328', 'bari', 'mufeed.kodika@gmail.com', '$2y$10$baJyF06Oq7UGdKeKl3oCR.4wfNFgRSOxifhYyKkw8/kiry68ZJ80u', '2025-02-04 20:43:01'),
+(5, 'Ahmed', 'nekura', '+249119766776', 'lamab', '7moody@gmail.com', '$2y$10$8eKqga/Kd1XLEeztrXq0veEDQfTOHa3ck02tyOP9d0T/qBH5CFAxy', '2025-02-15 13:35:30');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
